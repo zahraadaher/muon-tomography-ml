097bde8971f0f761b7bf35c31df36e6b6618ee9d
Zahraa Zaher (zahraa.zaher@uclouvain.be) <zdaher@mbackf2.cism.ucl.ac.be>
2026-08-21 18:24:13 +0200

    handling of tomopt prediction collate

--- src/data/collate.py
+++ src/data/collate.py
@@ -1,6 +1,6 @@
 from torch.nn.utils.rnn import pad_sequence
 import torch
-
+import numpy as np
 
 def collate_poca_batch(batch, voxel_shape=None):
 
@@ -12,7 +12,7 @@ def collate_poca_batch(batch, voxel_shape=None):
     ]
 
     targets = [ 
-        item["target"].view(C, H, W).permute(1, 2, 0)
+        item["target"].view(W, C, H).permute(1, 2, 0)  # target grids are stored as ZXY in tomopt
         for item in batch
     ]
 
@@ -39,6 +39,22 @@ def collate_poca_batch(batch, voxel_shape=None):
         lengths[:,None]
     )
 
+    # include baseline x0 prediction from tomopt if available
+    if "prediction" in batch[0]:
+        preds = [
+            item["prediction"].view(W, C, H).permute(1, 2, 0)  # similarly to target
+            for item in batch
+            ]
+        
+        return {
+                "features": padded_features,
+                "target": torch.stack(targets),
+                "mask": mask,
+                "material_id": torch.tensor(material_ids),
+                "prediction": torch.stack(preds)
+            }
+        
+   
     return {
         "features": padded_features,
         "target": torch.stack(targets),
