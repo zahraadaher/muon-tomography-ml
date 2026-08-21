53bf5d25dd3d447628a2371bc035f5b35c7a755f
Zahraa Zaher (zahraa.zaher@uclouvain.be) <zdaher@mbackf2.cism.ucl.ac.be>
2026-08-21 18:23:31 +0200

    minor fixes

--- src/data/tomopt.py
+++ src/data/tomopt.py
@@ -6,7 +6,7 @@ from .base import BaseDataModule
 from .readers.hdf5_reader import HDF5Reader
 from .dataset import MuonTomographyDataset
 from .splits.balanced_material_position import BalancedMaterialPositionSplit
-from .transforms import NormalizeFeatures, LogTarget
+from .transforms import NormalizeFeatures, LogInverseTarget
 from src.utils.feat_statistics import compute_feature_stats
 
 
@@ -14,9 +14,11 @@ class TomOptDataModule(BaseDataModule):
 
     def __init__(
         self,
-        hdf5_path,
-        split_config,
-        in_memory=True
+        hdf5_path: str,
+        split_config: str,
+        in_memory: bool=True,
+        n_points = 20000,
+        normalize = False
     ):
 
         self.reader = HDF5Reader(hdf5_path)
@@ -29,91 +31,96 @@ class TomOptDataModule(BaseDataModule):
         self.val_indices = None
         self.test_indices = None
 
-        # number of sampled points
-        self.n_points = 20000
+        self.n_points = n_points
 
-    def prepare(self, normalize=False):
+        self.normalize = normalize
+
+    def prepare(self):
 
         # fix random seed for reproducibility of sampled poca events
         rng = np.random.default_rng(seed=42)
 
         # Load data cache
         logging.info("Preparing dataset")
-        for name in self.reader.list_samples():
 
-            # self.cached_data[name] = (
-            #     self.reader.get_sample(name)
-            # )
-            sample = self.reader.get_sample(name)
+        if self.in_memory:
+            with self.reader:  # <-- open the file ONCE for the whole loop
 
-            features = sample["features"]
+                for name in self.reader.list_samples():
 
-            # sampling points inside the voi
-            xyz = features[:, :3]  # poca x,y,z
+                    sample = self.reader.get_sample(name)
 
-            xmin, ymin, zmin = self.get_metadata()["voxel_origin"]
-            voxel_dims = self.get_metadata()["voxel_shape"]
-            voxel_size = self.get_metadata()["voxel_size"]
-            xmax = xmin + voxel_dims[0] * voxel_size
-            ymax = ymin + voxel_dims[1] * voxel_size
-            zmax = zmin + voxel_dims[2] * voxel_size
+                    features = sample["features"]
 
-            inside = (
-                (xyz[:, 0] >= xmin) & (xyz[:, 0] <= xmax) &
-                (xyz[:, 1] >= ymin) & (xyz[:, 1] <= ymax) &
-                (xyz[:, 2] >= zmin) & (xyz[:, 2] <= zmax)
+                    # sampling points inside the voi, using metadata of the tomopt dataset module defining grid shape ans voxel size
+                    xyz = features[:, :3]  # poca x,y,z
+
+                    xmin, ymin, zmin = self.get_metadata()["voxel_origin"]
+                    voxel_dims = self.get_metadata()["voxel_shape"]
+                    voxel_size = self.get_metadata()["voxel_size"]
+                    xmax = xmin + voxel_dims[0] * voxel_size
+                    ymax = ymin + voxel_dims[1] * voxel_size
+                    zmax = zmin + voxel_dims[2] * voxel_size
+
+                    inside = (
+                        (xyz[:, 0] >= xmin) & (xyz[:, 0] <= xmax) &
+                        (xyz[:, 1] >= ymin) & (xyz[:, 1] <= ymax) &
+                        (xyz[:, 2] >= zmin) & (xyz[:, 2] <= zmax)
+                    )
+
+                    features = features[inside]
+
+                    N = len(features)
+
+                    if N >= self.n_points:
+                        idx = rng.choice(N, self.n_points, replace=False)
+                    else:
+                        idx = rng.choice(N, self.n_points, replace=True)
+
+                    sample["features"] = features[idx]
+
+                    
+                    self.cached_data[name] = sample
+                    
+                logging.info(
+                    f"Loaded {len(self.cached_data)} samples"
+                )
+
+            # Create train/val/test split
+            splitter = BalancedMaterialPositionSplit(
+                n_positions=self.split_config.n_positions,
+                n_materials=self.split_config.n_materials,
+                n_val_pos=self.split_config.n_val_pos,
+                n_test_pos=self.split_config.n_test_pos,
+                random_seed=self.split_config.random_seed
             )
 
-            features = features[inside]
+            (
+                self.train_indices,
+                self.val_indices,
+                self.test_indices
+            ) = splitter.split(self.reader)
 
-            N = len(features)
-
-            if N >= self.n_points:
-                idx = rng.choice(N, self.n_points, replace=False)
+            # Compute feature statistics only if normalization is requested
+            # NOTE: since for reproducibility we are using fixed split dataset indices, the training stats are being computed
+            # using a utils function in the main training script
+            if self.normalize:
+                self.feature_mean, self.feature_std = compute_feature_stats(
+                    self.reader,
+                    self.train_indices
+                )
             else:
-                idx = rng.choice(N, self.n_points, replace=True)
+                self.feature_mean = None
+                self.feature_std = None
 
-            sample["features"] = features[idx]
-
-            self.cached_data[name] = sample
-            
-        logging.info(
-            f"Loaded {len(self.cached_data)} samples"
-        )
-
-        # Create train/val/test split
-        splitter = BalancedMaterialPositionSplit(
-            n_positions=self.split_config.n_positions,
-            n_materials=self.split_config.n_materials,
-            n_val_pos=self.split_config.n_val_pos,
-            n_test_pos=self.split_config.n_test_pos,
-            random_seed=self.split_config.random_seed
-        )
-
-        (
-            self.train_indices,
-            self.val_indices,
-            self.test_indices
-        ) = splitter.split(self.reader)
-
-        # Compute feature statistics only if normalization is requested
-        if normalize:
-            self.feature_mean, self.feature_std = compute_feature_stats(
-                self.reader,
-                self.train_indices
-            )
-        else:
-            self.feature_mean = None
-            self.feature_std = None
-
-    def create_datasets(self, normalize=False):
+    def create_datasets(self):
 
         train_transform = None
         val_transform = None
         test_transform = None
-        x0_transform = LogTarget()
+        x0_transform = LogInverseTarget()
 
-        if normalize:
+        if self.normalize:
             if self.feature_mean is None or self.feature_std is None:
                 raise RuntimeError(
                     "Normalization requested but feature statistics were not computed. "
@@ -134,7 +141,6 @@ class TomOptDataModule(BaseDataModule):
             feat_transform=train_transform,
             target_transform=x0_transform,
             cached_data=self.cached_data,
-            in_memory=self.in_memory
         )
 
         val_dataset = MuonTomographyDataset(
@@ -143,7 +149,6 @@ class TomOptDataModule(BaseDataModule):
             feat_transform=val_transform,
             target_transform=x0_transform,
             cached_data=self.cached_data,
-            in_memory=self.in_memory
         )
 
         test_dataset = MuonTomographyDataset(
@@ -152,7 +157,6 @@ class TomOptDataModule(BaseDataModule):
             feat_transform=test_transform,
             target_transform=x0_transform,
             cached_data=self.cached_data,
-            in_memory=self.in_memory
         )
 
         return (
