86c4550ed9227334e4f41f05b85c4cdbccef630d
Zahraa Zaher (zahraa.zaher@uclouvain.be) <zdaher@mbackf2.cism.ucl.ac.be>
2026-08-21 18:29:23 +0200

    type assignment and minor changes

--- src/data/factory.py
+++ src/data/factory.py
@@ -1,12 +1,18 @@
+from typing import Optional
+
 from .tomopt import TomOptDataModule
+from ..params import DataParams
 
 
-def get_dataset(config):
+def get_dataset(config: DataParams, n_points: Optional[int], normalize: Optional[bool], in_memory: Optional[bool]):
 
     if config.type == "tomopt":
         return TomOptDataModule(
             hdf5_path=config.path,
-            split_config=config.split
+            split_config=config.split,
+            n_points=n_points,
+            normalize=normalize,
+            in_memory=in_memory
         )
 
     raise ValueError(
